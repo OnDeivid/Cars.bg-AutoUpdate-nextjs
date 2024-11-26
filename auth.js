eec57b3ee5674df7c2f7e638b2a7c1229e46f66e
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-26 23:01:58 +0200

    add new third party providers

--- auth.js
+++ auth.js
@@ -1,11 +1,28 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import Facebook from 'next-auth/providers/facebook'
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/prismas"
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Facebook({
+      clientId:process.env.AUTH_FACEBOOK_ID,
+      clientSecret:process.env.AUTH_FACEBOOK_SECRET
+    })
+  ],
+
 })
