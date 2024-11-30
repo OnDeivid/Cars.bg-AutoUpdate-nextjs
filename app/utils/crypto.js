3666294416e4c41e1348784796c3ffba2190fd34
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-30 23:50:10 +0200

    implement encrypting for cars.bg password

---
+++ app/utils/crypto.js
@@ -0,0 +1,16 @@
+import crypto from 'crypto';
+
+const ENCRYPTION_KEY = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef';
+const ALGORITHM = 'aes-256-cbc';
+
+export async function encryptPassword(password) {
+    const iv = crypto.randomBytes(16);
+
+    const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+
+    let encrypted = cipher.update(password, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+
+    return `${iv.toString('base64')}:${encrypted}`;
+}
+
