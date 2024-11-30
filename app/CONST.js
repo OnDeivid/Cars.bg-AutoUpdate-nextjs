ddd577f87a6d9c4086ceea6e4c8f714983e6ffc3
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-30 23:11:50 +0200

    Create CONST.js

---
+++ app/CONST.js
@@ -0,0 +1,18 @@
+
+const endpoints = {
+    home: '/',
+    getStarted: '/GetStarted',
+    aboutUs: '/AboutUs',
+    guide: '/Guide',
+    login: '/Login'
+}
+const textEndpoint = {
+    home: 'Начало',
+    getStarted: 'Започни',
+    guide: 'Ръководство',
+    aboutUs: 'За Нас'
+}
+
+
+
+export { endpoints, textEndpoint };
