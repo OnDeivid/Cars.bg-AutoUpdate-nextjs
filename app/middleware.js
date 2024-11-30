470170280ca39786c347df3ee20f89da96ec15a0
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-30 23:47:22 +0200

    Create middleware.js

---
+++ app/middleware.js
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation"
+import { endpoints } from "./CONST"
+const { auth } = require("@/auth")
+
+async function Unauthenticated() {
+    const session = await auth()
+
+    if (!session || !session?.user) {
+        redirect(endpoints.login)
+    }
+    return null
+}
+
+
+async function Authenticated() {
+    const session = await auth()
+
+    if (session || session?.user) {
+        redirect(endpoints.home)
+    }
+    return null
+}
+
+export { Unauthenticated, Authenticated }
