db25c7666592d757db9b3d084702d85fd864860a
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-30 23:51:11 +0200

    Update route logic

---
+++ app/pages/api/CarsData/route.js
@@ -0,0 +1,33 @@
+import { encryptPassword } from "@/app/utils/crypto";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
+const prisma = new PrismaClient();
+
+export async function POST(req) {
+
+    try {
+        const { userEmail, carsEmail, password, confirmPassword } = await req.json();
+
+        if (password !== confirmPassword) {
+            return new Response(
+                JSON.stringify({ success: false, error: "Passwords do not match" }),
+                { status: 400 }
+            );
+        }
+
+        const data = await prisma.user.findUnique({ where: { email: userEmail }, select: { id: true } });
+        const userId = data.id
+
+        const hashedPassword = await encryptPassword(password)
+        console.log(hashedPassword)
+
+        const newEntry = await prisma.carsData.create({
+            data: { userId, userEmail, carsEmail, password: hashedPassword, confirmPassword },
+        });
+
+
+        return new Response(JSON.stringify({ success: true, data: newEntry }), { status: 201 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
+    }
+}
