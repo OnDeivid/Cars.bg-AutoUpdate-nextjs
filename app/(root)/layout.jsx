da3d1a9f0ffe4fe6ec323da6b8c7835e2efe8180
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-06 18:05:34 +0200

    Update the layout.jsx

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function layout({ children }) {
     return (
-        <div className='h-screen bg-gradient-to-t from-slate-900 to-black w-full'>
+        <div className='h-screen bg-custom-gray'>
             {children}
         </div>
 
