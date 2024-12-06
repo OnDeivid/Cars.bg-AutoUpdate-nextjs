ac86f63a06c58be5b302adc9212e6aa4ef861f4e
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-06 18:08:03 +0200

    Update home/page.jsx

--- app/(root)/home/page.jsx
+++ app/(root)/home/page.jsx
@@ -6,10 +6,7 @@ import Footer from "../../components/Footer";
 export default function Home() {
   return (
     <div className="text-black bg-slate-700">
-      <Head>
-        <title>nine4</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+      <Header />
       <Main />
       <Footer />
     </div>
