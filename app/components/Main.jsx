05180f188d8ab1bb260997f81be4d801b924d4e5
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-06 18:03:27 +0200

    Update Main.jsx

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,39 +1,77 @@
-import { auth } from "@/auth";
 import Link from "next/link";
+import { auth } from "@/auth";
 
+import { endpoints } from "../CONST";
+
+import UpdateButton from "./UpdateButton";
 
 export default async function Main() {
   const session = await auth();
-  console.log(session)
+  const carsEmail = session?.user?.userDataCars?.carsEmail;
+
   return (
-    <section className="text-gray-6200 body-font">
-      <div className="max-w-2xl pt-52 pb-24 mx-auto">
-        <h1 className="text-center font-bold text-white mb-6 text-4xl">
+    <section className="text-gray-600  bg-gradient-to-t bg-custom-gray h-screen body-font">
+      <div className="max-w-6xl pt-40 border-custom-input-color rounded-3xl border-y-2 w-full bg-red pb-24 mx-auto">
+        <h1 className="text-center font-bold text-custom-white mb-6 text-2xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-6xl">
           Премахни скучните повтарящи се действия с нас
         </h1>
         <br></br>
-        <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
+        <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-600 text-center">
           ние ще опростим деня ти
           <br />
           нека свършим скучните неща вместо теб
         </h2>
         <br></br>
+        <div className="ml-0 text-center">
 
-        <div className="ml-6 text-center">
-          <a
-            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline" href="/">
-            <div className="flex text-lg">
-              <span className="justify-center">Прочети за нас </span>
-            </div>
-          </a>
-          
-          {session?.user ? (null) :
-            (<Link className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-1 bg-gradient-to-r from-blue-500 to-blue-800 px-16 text-md md:mt-0 focus:shadow-outline" href={'/login'} >
-              <div className="flex text-lg" href='/login'>
-                <span className="justify-center">Влез</span>
-              </div>
-            </Link>)
-          }
+          {carsEmail ? (
+            <>
+              <Link
+                className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline" href={endpoints.guide}>
+                <div className="flex text-lg">
+                  <span className="justify-center">Ръководство</span>
+                </div>
+              </Link>
+              <UpdateButton />
+            </>
+          ) : session?.user ? (
+            <>
+              <Link
+                className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
+                href={endpoints.home}>
+                <div className="flex text-lg">
+                  <span className="justify-center">Прочети за нас </span>
+                </div>
+              </Link>
+              <Link
+                className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition-colors duration-700 ease-in-out transform bg-transparent ml-1 bg-blue-600 px-16 text-md md:mt-0 focus:shadow-outline focus:border focus:border-blue-500 hover:bg-custom-input-color"
+                href={endpoints.getStarted}
+              >
+                <div className="flex text-lg">
+                  <span className="justify-center">Започни сега</span>
+                </div>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
+                href={endpoints.home}>
+                <div className="flex text-lg">
+                  <span className="justify-center">Прочети за нас </span>
+                </div>
+              </Link>
+              <Link
+                className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition-colors duration-700 ease-in-out transform bg-transparent ml-1 bg-blue-600 px-16 text-md md:mt-0 focus:shadow-outline focus:border focus:border-blue-500 hover:bg-custom-input-color"
+                href={endpoints.login}
+              >
+                <div className="flex text-lg">
+                  <span className="justify-center">Влез</span>
+                </div>
+              </Link>
+            </>
+          )}
+
         </div>
       </div>
       <div className="container flex flex-col items-center justify-center mx-auto">
