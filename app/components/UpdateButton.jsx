937f6e356dff44f69c636ca67029b080ac3d96f1
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-06 18:07:44 +0200

    Update UpdateButton.jsx

--- app/components/UpdateButton.jsx
+++ app/components/UpdateButton.jsx
@@ -1,19 +1,59 @@
-import { auth } from '@/auth';
 import React from 'react'
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
+import { endpoints } from '../CONST';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 export default async function UpdateButton() {
+
     const session = await auth()
+    const carsEmail = session?.user?.userDataCars?.carsEmail
+    const userEmail = session?.user.email
+
     const onUpdate = async () => {
         'use server'
-        const email = session?.user.email
-        const response = await fetch(`http://localhost:3001/Update/${email}`, {
+        const token = await prisma.carsData.findFirst({
+            where: {
+                userEmail: userEmail,
+            },
+            select: {
+                userId: true,
+            },
+        });
+        const { access_token } = await prisma.account.findFirst({
+            where: {
+                userId: token.userId,
+            },
+            select: {
+                access_token: true,
+            },
+        });
+
+
+        if (userEmail) {
+            if (!carsEmail) {
+                redirect(endpoints.getStarted)
+            }
+        }
+
+        if (!userEmail) {
+            redirect('/Login')
+        }
+
+        await fetch(`http://localhost:3001/Update/${userEmail}`, {
             method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${access_token}`  // Sending token in header
+            },
         });
     }
     return (
         <button
             onClick={onUpdate}
-            className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-1 bg-gradient-to-r from-orange-500 to-orange-800 px-12 text-md md:mt-0 focus:shadow-outline"
+            className="inline-flex items-center py-3  hover:bg-custom-input-color font-semibold tracking-tighter text-white transition-all duration-1000 ease-in-out transform bg-transparent ml-1 bg-orange-600 px-12 text-md md:mt-0 focus:shadow-outline focus:border focus:border-orange-500"
         >
             <div className="flex text-lg">
                 <span className="justify-center">Актолизиране</span>
