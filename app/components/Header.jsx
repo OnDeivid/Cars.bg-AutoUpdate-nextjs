81d2ade7e51da3e011164356deeb790acbfa933a
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-06 18:07:18 +0200

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,12 @@
 import InteractiveHeader from "./InteractiveHeader";
 import AuthActionButton from "./AuthActionButton";
+import HeaderOptions from "./HeaderOptions";
 
 export default async function Header() {
 
   return (
     <InteractiveHeader >
+      <HeaderOptions />
       <AuthActionButton />
     </InteractiveHeader>
   );
