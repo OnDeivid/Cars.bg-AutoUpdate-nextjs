93f34231ac5d551e00206929db5fe8a13f0ad3c3
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-08 17:46:40 +0200

    implement AuthActionButton.jsx component

--- app/components/AuthActionButton.jsx
+++ app/components/AuthActionButton.jsx
@@ -1,23 +1,30 @@
-// pages/HeaderUserState.js (server-side component)
-import { auth, signOut } from '@/auth';
 import Link from 'next/link';
 
+import { auth, signOut } from '@/auth';
+import { endpoints } from '../CONST';
+import UserImage from './UserImage';
+
 export default async function AuthActionButton() {
-    const session = await auth(); // Fetch server-side authentication state
+    const session = await auth();
+
     return (
-        <div className="md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex flex-wrap items-center md:text-base text-xs md:justify-center space-x-2">
+        <div className="md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex items-center md:text-wrap text-xs md:justify-center space-x-2">
             {session?.user ? (
-                <form action={async () => {
-                    'use server'
-                    await signOut({ redirectTo: '/' })
-                }}>
-                    <button className="cursor-pointer text-gray-300 hover:text-white hover:underline hover:bg-gray-800 pl-3 pr-3 pt-2 pb-2 font-semibold tr04 uppercase">
-                        logout
-                    </button>
-                </form>
+                <>
+                    {/* <img className="w-7 h-7 rounded-full" src={session.user.image} alt="Rounded avatar"></img> */}
+                    <UserImage image={session.user.image} />
+                    <form action={async () => {
+                        'use server'
+                        await signOut({ redirectTo: endpoints.home })
+                    }}>
+                        <button className="cursor-pointer text-gray-300 hover:text-white hover:underline pl-3 pr-3 pt-2 pb-2 font-semibold tr04 uppercase">
+                            изход
+                        </button>
+                    </form>
+                </>
             ) : (
-                <Link href='/login' className="cursor-pointer text-gray-300 hover:text-white hover:underline hover:bg-gray-800 pl-3 pr-3 pt-2 pb-2 font-semibold tr04 uppercase">
-                    login
+                <Link href={endpoints.login} className="cursor-pointer text-gray-300 hover:text-white hover:underline pl-3 pr-3 pt-2 pb-2 font-semibold tr04 uppercase">
+                    влез
                 </Link>
             )}
         </div>
