593c66940d2dddf9f620555eaa770464c9f11fc1
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-08 03:09:41 +0200

    finish the Guide page.jsx

--- app/(root)/guide/page.jsx
+++ app/(root)/guide/page.jsx
@@ -1,51 +1,58 @@
-import React from 'react'
-import audi from './../../../public/audi.png'
-export default function page() {
+import React from 'react';
+
+export default function Page() {
     return (
+        <div className="w-[90%] z-7 mt-14 mx-auto relative" id="default-carousel" data-carousel="slide">
+            <h1 className="text-custom-white pb-12 text-center text-2xl sm:text-3xl md:text-6xl">
+                Как да се регистрирам
+            </h1>
 
-
-        <div id="default-carousel" className="relative w-full" data-carousel="slide">
-            <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="./../" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+            {[
+                {
+                    step: "Първа стъпка:",
+                    description: 'да влезете в профила си кликнете "Влез"',
+                    image: '/1.png',
+                },
+                {
+                    step: "Втора стъпка:",
+                    description: "Изберете с какво да влезете",
+                    image: '/2.png',
+                },
+                {
+                    step: "Трета стъпка:",
+                    description:
+                        'Оверете се че Името което ще запишете и Паролата която ще запишете съответства на паролата и името с което влизате в cars.bg"',
+                    image: '/3.png',
+                },
+                {
+                    step: "Четвърта стъпка:",
+                    description: "Влезте отново със същата опция която избрахте първият път",
+                    image: '/4.png',
+                },
+                {
+                    step: "Пета стъпка:",
+                    description: 'Кликнете върху "Актуализиране"',
+                    image: '/5.png',
+                },
+            ].map((item, index) => (
+                <div
+                    key={index}
+                    className="relative rounded-lg mt-8"
+                >
+                    <p className="text-center px-4 pb-10 text-lg sm:text-xl md:text-2xl text-custom-white">
+                        <span className="font-bold">{item.step} </span>
+                        {item.description}
+                    </p>
+                    <div className="relative w-full mx-auto">
+                        <img
+                            src={item.image}
+                            className="w-full h-[auto] object-cover rounded-md"
+                            alt={`Step ${index + 1}`}
+                        />
+                    </div>
+                    <div className="bg-white w-full h-1 mx-auto mt-4 p-6"></div>
                 </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src='./audi.png' className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://myve.bg/f/news/b/34/75ec29c26760e927d8a67ccdec6ccd59.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://myve.bg/f/news/b/34/75ec29c26760e927d8a67ccdec6ccd59.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="https://myve.bg/f/news/b/34/75ec29c26760e927d8a67ccdec6ccd59.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-            </div>
-            <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-            </div>
-            <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" linecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-                    </svg>
-                    <span className="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-                    </svg>
-                    <span className="sr-only">Next</span>
-                </span>
-            </button>
+            ))}
         </div>
-
-    )
+    );
 }
