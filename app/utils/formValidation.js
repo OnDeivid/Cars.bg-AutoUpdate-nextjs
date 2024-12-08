95d58bdab7279caa0e640829a4cc9b4deb079103
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-08 17:49:41 +0200

    Create formValidation.js

---
+++ app/utils/formValidation.js
@@ -0,0 +1,21 @@
+export default function formValidation(formData) {
+
+    if (!formData?.carsEmail || formData?.carsEmail.trim() === "") {
+        return true
+    }
+    if (!formData.password || formData.password.length < 8) {
+        return true
+    } else if (!/[A-Za-z]/.test(formData.password)) {
+        return true
+    } else if (!/[0-9]/.test(formData.password)) {
+        return true
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(formData.password)) {
+        return true
+    }
+
+    if (formData.confirmPassword !== formData.password) {
+        return true
+    }
+
+    return false
+}
