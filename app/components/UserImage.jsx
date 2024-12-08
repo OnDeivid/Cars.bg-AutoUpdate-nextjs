099d14f0c74a8cecaf6f13da1362c0292aeeefd5
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-08 17:48:51 +0200

    Create UserImage.jsx

---
+++ app/components/UserImage.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function UserImage({ image }) {
+
+    return (
+        <div>
+            <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="w-7 h-7 rounded-full cursor-pointer" src={image} alt="User dropdown" />
+            <div id="userDropdown" className="z-8 flex right-4 absolute mt-4 bg-white divide-y divide-gray-100 rounded-lg shadow w-22 dark:bg-custom-gray dark:divide-gray-200">
+                <ul className="py-2 text-sm text-gray-700 dark:text-custom-white" aria-labelledby="avatarButton">
+                    <li>
+                        <Link href="/Settings" className="block px-4 py-2 text-center hover:bg-gray-100 dark:hover:bg-custom-input-color duration-500 dark:hover:text-custom-gray">Настройки</Link>
+                    </li>
+                </ul>
+            </div>
+        </div >
+    )
+}
