d870bc8321fe18423c3ab469b87f561e6069c158
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-09 03:27:33 +0200

    remove comment from AuthActionButton.jsx

--- app/components/AuthActionButton.jsx
+++ app/components/AuthActionButton.jsx
@@ -12,7 +12,6 @@ export default async function AuthActionButton() {
         <div className="md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex items-center md:text-wrap text-xs md:justify-center space-x-2">
             {session?.user ? (
                 <>
-                    {/* <img className="w-7 h-7 rounded-full" src={session.user.image} alt="Rounded avatar"></img> */}
                     <UserImage image={session.user.image} />
                     <form action={async () => {
                         'use server'
