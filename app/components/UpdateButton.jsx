7b28f73ab0e0c6bb4f16e4e4b0e088e77856a17e
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-09 23:02:06 +0200

    make the code a lil more secure

--- app/components/UpdateButton.jsx
+++ app/components/UpdateButton.jsx
@@ -1,38 +1,22 @@
-import React from 'react'
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
-
-import { endpoints } from '../CONST';
-
 import { PrismaClient } from '@prisma/client';
 
+import { endpoints } from '../CONST';
+import UpdateButtonState from './UpdateButtonState';
+
 const prisma = new PrismaClient();
+
 export default async function UpdateButton() {
 
     const session = await auth()
+
     const carsEmail = session?.user?.userDataCars?.carsEmail
     const userEmail = session?.user.email
 
     const onUpdate = async () => {
         'use server'
-        const token = await prisma.carsData.findFirst({
-            where: {
-                userEmail: userEmail,
-            },
-            select: {
-                userId: true,
-            },
-        });
-        const { access_token } = await prisma.account.findFirst({
-            where: {
-                userId: token.userId,
-            },
-            select: {
-                access_token: true,
-            },
-        });
-
 
         if (userEmail) {
             if (!carsEmail) {
@@ -44,22 +28,21 @@ export default async function UpdateButton() {
             redirect('/Login')
         }
 
-        await fetch(`http://localhost:3001/Update/${userEmail}`, {
-            method: 'GET',
+        const response = await fetch(`http://localhost:3000/pages/api/OnCarsUpdate`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${access_token}`  // Sending token in header
             },
+            body: JSON.stringify({ session: session }),
         });
+        console.log(response)
     }
     return (
-        <button
-            onClick={onUpdate}
-            className="inline-flex items-center py-3 hover:bg-custom-input-color font-semibold tracking-tighter text-white transition-all duration-1000 ease-in-out transform ml-1 bg-orange-600 px-12 text-md md:mt-0 focus:shadow-outline focus:border focus:border-orange-500"
-        >
-            <div className="flex text-lg">
-                <span className="justify-center">Актулизиране</span>
-            </div>
-        </button>
+        // <button
+        //     onClick={onUpdate}
+        //     className="inline-flex items-center py-3 hover:bg-custom-input-color font-semibold tracking-tighter text-white transition-all duration-1000 ease-in-out transform ml-1 bg-orange-600 px-12 text-md md:mt-0 focus:shadow-outline focus:border focus:border-orange-500"
+        // >
+            <UpdateButtonState update={onUpdate} />
+        // </button>
     )
 }
