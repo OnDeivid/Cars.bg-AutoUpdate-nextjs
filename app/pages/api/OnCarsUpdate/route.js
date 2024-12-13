d7bf6470d0ab8cddc6f19590530788704dbe50ee
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-13 16:12:50 +0200

    Update route.js

--- app/pages/api/OnCarsUpdate/route.js
+++ app/pages/api/OnCarsUpdate/route.js
@@ -1,9 +1,8 @@
-import { auth } from "@/auth";
 import { PrismaClient } from "@prisma/client";
 
 export async function POST(req) {
     const { session } = await req.json()
-   
+
     const userEmail = session?.user.email
 
     const prisma = new PrismaClient();
@@ -26,8 +25,7 @@ export async function POST(req) {
         },
     });
     try {
-
-        const response = await fetch(`http://localhost:3001/Update/${userEmail}`, {
+        await fetch(`http://localhost:3001/Update/${userEmail}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -41,7 +39,4 @@ export async function POST(req) {
 
         return new Response(JSON.stringify({ message: 'Request for update sent Unsuccessfully !!!' }), { status: 400 });
     }
-
-
-
 }
