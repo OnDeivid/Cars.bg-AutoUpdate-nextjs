f91e2a3786bdacfb23c0913df08dcd35449dfc94
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-14 09:35:38 +0200

    Implement OnLocalStorageDelete.jsx
    change the name late dave!

--- app/components/OnLocalStorageDelete.jsx
+++ app/components/OnLocalStorageDelete.jsx
@@ -1,14 +1,34 @@
-'use client'
-import React from 'react'
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export default function OnLocalStorageDelete({ onNextDay }) {
-    if (onNextDay) {
-        console.log('data has been deleted!')
-        localStorage.removeItem('updateCars')
-    }
-    return (
-        <div>
+    const router = useRouter();
 
-        </div>
-    )
+    useEffect(() => {
+        if (onNextDay) {
+            console.log('Data has been deleted!');
+            localStorage.removeItem('updateCars');
+        }
+
+        const intervalId = setInterval(() => {
+            const updateDate = localStorage.getItem('updateDate');
+            const currentDate = new Date().toDateString();
+
+
+
+            console.log('Client',new Date(updateDate) < new Date(currentDate))
+            if (new Date(updateDate) < new Date(currentDate)) {
+                localStorage.setItem('updateDate', currentDate);
+                localStorage.removeItem('updateCars');
+            }
+            router.push('/');
+        }, 10000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [onNextDay, router]);
+
+    return (<div></div>)
 }
