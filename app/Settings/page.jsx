4d16adcc00139286d5506578c92328beff9906b5
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-22 22:29:20 +0200

    Update page.jsx

--- app/Settings/page.jsx
+++ app/Settings/page.jsx
@@ -1,13 +1,48 @@
-import { auth } from '@/auth'
-import React from 'react'
+'use server'
+
+import { auth, signOut } from '@/auth'
+import formValidation from '../utils/formValidation';
+import { endpoints } from '../CONST';
 
 export default async function page() {
     const session = await auth()
+
+    async function handleSubmit(formData) {
+        'use server';
+        const email = formData.get('carsEmail');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirmPassword');
+
+
+        const formValue = {
+            userEmail: session?.user?.email,
+            carsEmail: email,
+            password: password,
+            confirmPassword: confirmPassword,
+        };
+
+        if (formValidation(formValue)) {
+            return
+        }
+
+
+        await fetch('http://localhost:3000/pages/api/UserUpdate', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formValue),
+        });
+
+        await signOut({ redirectTo: endpoints.login });
+    }
     return (
         <>
             <div className="mx-4 min-h-screen max-w-screen-xl text-custom-light-gray sm:mx-8 xl:mx-auto">
                 <h2 className="relative py-6 text-xl font-semibold text-center"></h2>
+                <h1 className='text-3xl text-center text-white py-3'>Промени Потребителското Име или Паролата</h1>
                 <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
+
                     <div className="relative my-4 w-56 sm:hidden">
                         <input className="peer hidden" type="checkbox" name="select-1" id="select-1" />
                         <label htmlFor="select-1" className="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-blue-700 peer-checked:ring">Accounts </label>
@@ -27,35 +62,28 @@ export default async function page() {
                         </ul>
                     </div>
 
-                    <div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
-                        <div className="pt-4">
-                            <h1 className="py-2 text-2xl font-semibold">Настройки</h1>
-                            <p className="text-gray-600">Емайл: <strong>{session?.user.email}</strong></p>
-                        </div>
-                        <hr className="mt-4 mb-8" />
-                        <p className="py-2 text-xl font-semibold">Потребителско Име</p>
-                        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                            <p className="text-gray-600">Потребителско име: <strong>{session?.user.userDataCars.carsEmail}</strong></p>
-                            <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">Промени</button>
-                        </div>
-                        <hr className="mt-4 mb-8" />
-                        <p className="py-2 text-xl font-semibold">Парола</p>
-                        <div className="flex items-center">
-                            <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-                                <label htmlFor="login-password">
-                                    <span className="text-sm text-gray-500">Нова Парола</span>
-                                    <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                                        <input type="password" id="login-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
-                                    </div>
-                                </label>
+                    <div className="col-span-8 overflow-hidden rounded-xl sm:bg-slate-50 py-20 sm:px-8 sm:shadow">
+
+
+                        <form action={handleSubmit} className="max-w-sm mx-auto">
+                            <div className="mb-5">
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-custom-gray">Потребителско Име</label>
+                                <input type="username" id="carsEmail" name='carsEmail' className="bg-gray-50 border border-gray-300  text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={session?.user?.userDataCars?.carsEmail} required />
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 
-                                4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
-                            </svg>
-                        </div>
-                        <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
-                        <hr className="mt-4 mb-8" />
+                            <div className="mb-5">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-custom-gray">Парола</label>
+                                <input type="password" id="password" name='password' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-custom-gray">Потвърди Паролата</label>
+                                <input type="password" id="confirmPassword" name='confirmPassword' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            </div>
+                            <div className="flex items-start mb-5">
+                                <br />
+                            </div>
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Промени</button>
+                        </form>
+
 
                     </div>
                 </div>
