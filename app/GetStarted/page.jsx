2895e2c6d744b330c3c4d850eb92f5fd013a4e4f
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-25 22:27:39 +0200

    Update GetStarted page.jsx

--- app/GetStarted/page.jsx
+++ app/GetStarted/page.jsx
@@ -31,7 +31,7 @@ export default async function page() {
       return
     }
 
-    await fetch('http://localhost:3000/pages/api/CarsData', {
+    await fetch('https://automation-eosin.vercel.app/pages/api/CarsData', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
