479a3cd6e143c42cac2a0d16419ddb87d86cf20a
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-25 20:15:54 +0200

    update auth.js

--- auth.js
+++ auth.js
@@ -38,7 +38,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         }
       }catch(err)
       {
-        console.log(err)
+        console.error(err);
       }
         return token;
       },
