2b3e0249a12c3bf2318ca480b7f316cdbf65d387
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-26 14:26:13 +0200

    return to the old css for GetStarted button

--- app/components/GetStartedLoginButton.jsx
+++ app/components/GetStartedLoginButton.jsx
@@ -18,7 +18,7 @@ export default function GetStartedLoginButton() {
         <>
             {!onShowLoading ? (
                 <div onClick={() => { setShowLoading(true) }}
-                    className="w-full dark:border-y-white relative -z-10 text-gray dark:bg-custom-white hover:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    className="w-full dark:border-y-white relative text-gray dark:bg-custom-white hover:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
                     Sign in
                 </div>
