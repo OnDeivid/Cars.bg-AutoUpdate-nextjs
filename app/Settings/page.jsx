0650a2a747696fac1e45f3d67d77bf1ca88d5b24
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-28 05:39:31 +0200

    Update page.jsx Settings

--- app/Settings/page.jsx
+++ app/Settings/page.jsx
@@ -71,11 +71,11 @@ export default async function page() {
                             </div>
                             <div className="mb-5">
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-custom-gray">Парола</label>
-                                <input type="password" id="password" name='password' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                <input type="username" id="password" name='password' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <div className="mb-5">
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-custom-gray">Потвърди Паролата</label>
-                                <input type="password" id="confirmPassword" name='confirmPassword' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                <input type="username" id="confirmPassword" name='confirmPassword' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <div className="flex items-start mb-5">
                                 <br />
