724df14b19c24e7d976fd5406626501a9b7d1108
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-30 21:55:42 +0200

    Update FormInputsValue.jsx

--- app/components/FormInputsValue.jsx
+++ app/components/FormInputsValue.jsx
@@ -30,12 +30,12 @@ export default function FormInputsValue({ userEmail_B, carsEmail_B }) {
     useFormValidation(formValue);
     return (
         <>
-            {userEmail_B ? <h3 className='text-gray-800'>Email: <span className='text-black font-medium'>{userEmail_B}</span></h3> : null}
+            {userEmail_B ? <h3 className='relative text-gray-800 -top-10'>Email: <span className='text-black font-medium'>{userEmail_B}</span></h3> : null}
 
             <div>
                 <label
                     htmlFor="carsEmail"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
                 >
                     Името с което влизаш в cars.bg
                 </label>
@@ -48,7 +48,7 @@ export default function FormInputsValue({ userEmail_B, carsEmail_B }) {
                     className={!userEmail_B ? "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-custom-white dark:border-gray-600 dark:placeholder-gray-800 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" :
                         "bg-gray-50 border border-gray-300  text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:text-white"
                     }
-                    placeholder="Your Email, Username or Number"
+                    placeholder="Your Cars.bg Username"
                     value={formValue.carsEmail}
 
                     required
@@ -59,7 +59,7 @@ export default function FormInputsValue({ userEmail_B, carsEmail_B }) {
             <div>
                 <label
                     htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className={`block mb-2 text-sm font-medium text-gray-900 ${carsEmail_B ? 'dark:text-gray-900' : 'dark:text-white'}`}
                 >
                     Паролата с която влизаш в cars.bg
                 </label>
@@ -81,7 +81,7 @@ export default function FormInputsValue({ userEmail_B, carsEmail_B }) {
             <div>
                 <label
                     htmlFor="confirmPassword"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className={`block mb-2 text-sm font-medium text-gray-900 ${carsEmail_B ? 'dark:text-gray-900' : 'dark:text-white'}`}
                 >
                     Потвърди паролата с която влизаш в cars.bg
                 </label>
