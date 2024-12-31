aa9783ffbe02c334a410e1e4b0e20ad1e7b45441
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-31 14:34:03 +0200

    Update GetStartedLoginButton.jsx

--- app/components/GetStartedLoginButton.jsx
+++ app/components/GetStartedLoginButton.jsx
@@ -15,19 +15,19 @@ export default function GetStartedLoginButton() {
     }, [onShowLoading]);
 
     return (
-        <button type='submit' className='w-full bg-red-800'>
+        <button type='submit' onClick={() => {
+            if (!onShowLoading) setShowLoading(true);
+        }}
+            className='w-full dark:border-y-white relative text-gray dark:bg-custom-white hover:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
 
-            {!onShowLoading ?
-                <div
-                    onClick={() => {
-                        if (!onShowLoading) setShowLoading(true);
-                    }}
-                    disabled={onShowLoading}
-                    className="w-full dark:border-y-white relative text-gray dark:bg-custom-white hover:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                    Sign in
-                </div>
-                : null}
+            <div
+                onClick={() => {
+                    if (!onShowLoading) setShowLoading(true);
+                }}
+                className=""
+            >
+                Sign in
+            </div>
         </button>
     );
 }
