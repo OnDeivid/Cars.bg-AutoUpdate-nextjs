7f2b9cdb747c60bccef0e48873f74de47d116d32
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-12-31 15:15:47 +0200

    Update SettingsButtonSubmit.jsx

--- app/components/SettingsButtonSubmit.jsx
+++ app/components/SettingsButtonSubmit.jsx
@@ -24,11 +24,11 @@ export default function SettingsButtonSubmit() {
             }}
             className={
                 !state
-                    ? "text-white bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    ? "text-white bg-blue-700 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full mt-3 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     : "hidden"
             }
         >
-            <div>Sign in</div>
+            Промени
         </button>
     );
 }
