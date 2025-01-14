209d6c5e1c56edf2984466ffc656865ad6e127de
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2025-01-14 14:54:58 +0200

    Update GetStartedLoginButton.jsx

--- app/components/GetStartedLoginButton.jsx
+++ app/components/GetStartedLoginButton.jsx
@@ -18,8 +18,8 @@ export default function GetStartedLoginButton() {
         <button type='submit' onClick={() => {
             if (!onShowLoading) setShowLoading(true);
         }}
-            className={!onShowLoading?'w-full dark:border-y-white relative text-gray dark:bg-custom-white hover:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-800':
-                'hidden'
+            className={!onShowLoading ? 'w-full relative text-black bg-white hover:bg-gray-100 border border-gray-300 dark:border-y-white dark:text-gray dark:bg-custom-white hover:dark:bg-custom-input-color transition-all focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+                : 'hidden'
             }>
 
             <div
